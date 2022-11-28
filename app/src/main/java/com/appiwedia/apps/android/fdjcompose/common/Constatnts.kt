2af6d80dacfca020cc7b5465eb40357da27eeb7b
Clark <e.cleawrence@gmail.com>
2022-11-28 12:25:05 +0100

    create constants object

---
+++ app/src/main/java/com/appiwedia/apps/android/fdjcompose/common/Constatnts.kt
@@ -0,0 +1,6 @@
+package com.appiwedia.apps.android.fdjcompose.common
+
+object Constants {
+    const val BASE_URL = "https://www.thesportsdb.com/api/v1/json/50130162/"
+    const val PARAM_TEAM_NAME = "teamName"
+}
